68223157b9b4a9777343eadee411a1c03a3c0408
srihitha2318 <srihithareddy2318@gmail.com>
2025-11-24 12:45:08 +0530

    first commit

---
+++ Day_7/Q19.js
@@ -0,0 +1,18 @@
+
+let age = 20;
+
+
+function displayAge() {
+  console.log("Age is:", age);
+}
+
+
+function changeAge() {
+  age = 25; 
+  console.log("Age updated to:", age);
+}
+
+
+displayAge();   
+changeAge();    
+displayAge();   
