868a485302eff5854f0d26277d1f77950cfd088b
srihitha2318 <srihithareddy2318@gmail.com>
2025-11-24 11:12:18 +0530

    my fourth  commit

---
+++ Day_5/Q14.js
@@ -0,0 +1,12 @@
+const profile = { 
+  name: "Charlie", 
+  age: 29, 
+  address: { city: "San Francisco", zipcode: "94101" } 
+};
+
+const updates = { 
+  age: 30, 
+  address: { zipcode: "94109", country: "USA" } 
+};
+
+console.log(profile, updates);
