994b178c23086f8f15a699fc0be648248df6d336
srihitha2318 <srihithareddy2318@gmail.com>
2025-11-24 11:10:25 +0530

    my third  commit

---
+++ Day_5/Q13.js
@@ -0,0 +1,18 @@
+const people = [
+  { 
+    name: "Alice",
+    address: { 
+      city: "New York",
+      street: { name: "Broadway", number: 123 }
+    }
+  },
+  {
+    name: "Bob",
+    address: { 
+      city: "Los Angeles",
+      street: { name: "Sunset Boulevard", number: 456 }
+    }
+  }
+];
+
+console.log(people);
