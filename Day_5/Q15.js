6b0c4b6dcdc76c67b5c0f690cbefe6d07cced43c
srihitha2318 <srihithareddy2318@gmail.com>
2025-11-24 11:16:16 +0530

    my fifth  commit

---
+++ Day_5/Q15.js
@@ -0,0 +1,17 @@
+const username = "Sam";
+const course = "JavaScript";
+
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+
+const name = "Sam";
+const age = 21;
+
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+
+const getFullName = (first, last) => `${first} ${last}`;
