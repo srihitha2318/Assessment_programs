352139bc81bcc969196200cf8d15f5d1945d5d8c
srihitha2318 <srihithareddy2318@gmail.com>
2025-12-18 22:02:32 +0530

    Day 3

---
+++ React/Day_3/Q1/src/UserData.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function UserData() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
+
+  return (
+    <div>
+      <h2>User Details</h2>
+      <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Phone:</strong> {user.phone}</p>
+    </div>
+  );
+}
+
+export default UserData;
